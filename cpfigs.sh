64cf8d2ed1f7dc5573dcfb56268a8c07df5e7e11
Bob Weigel <rweigel@gmu.edu>
2024-04-09 11:35:18 -0400

    updated figures

--- cpfigs.sh
+++ cpfigs.sh
@@ -1,2 +1,5 @@
+git pull
 cp ../tfs-20120712-20121107/figures/tsplot-original-Middelpos-tf1.pdf figures/
 cp ../tfs-20120712-20121107/figures/dftplot-original-averaged-magnitudes-Middelpos-tf1.pdf figures/
+git commit -a -m "updated figures"
+git push
