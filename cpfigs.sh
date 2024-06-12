e21a2e16ccee1cf7bd3c446a76836b36e346d698
Bob Weigel <rweigel@gmu.edu>
2024-06-12 09:37:31 -0400

    updated figures

--- cpfigs.sh
+++ cpfigs.sh
@@ -6,6 +6,8 @@ cp $DIR/zplot-magnitude_phase-Middelpos-tf1-Z_xx.pdf figures/
 cp $DIR/zplot-magnitude_phase-Middelpos-tf1-Z_xy.pdf figures/
 cp $DIR/zplot-magnitude_phase-Middelpos-tf1-Z_yy.pdf figures/
 cp $DIR/zplot-magnitude_phase-Middelpos-tf1-Z_yx.pdf figures/
+cp $DIR/snplot-Middelpos-tf1-E_x.pdf figures/
+cp $DIR/snplot-Middelpos-tf1-E_y.pdf figures/
 git add -A figures/
 git commit -a -m "updated figures"
 git push
