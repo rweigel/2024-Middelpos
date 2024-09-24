0e2fa603d9b7f0738946bb5788e7e2c9c2670f4c
Bob Weigel <Robert.S.Weigel@gmail.com>
2024-09-24 15:01:16 -0400

    updated figures

--- cpfigs.sh
+++ cpfigs.sh
@@ -2,12 +2,12 @@ git pull
 DIR=../tfs-20120712-20121107/figures
 cp $DIR/tsplot-original-Middelpos-tf1.pdf figures/
 cp $DIR/dftplot-original-averaged-magnitudes-Middelpos-tf1.pdf figures/
-cp $DIR/zplot-magnitude_phase-Middelpos-tf1-Z_xx.pdf figures/
-cp $DIR/zplot-magnitude_phase-Middelpos-tf1-Z_xy.pdf figures/
-cp $DIR/zplot-magnitude_phase-Middelpos-tf1-Z_yy.pdf figures/
-cp $DIR/zplot-magnitude_phase-Middelpos-tf1-Z_yx.pdf figures/
-cp $DIR/snplot-Middelpos-tf1-E_x.pdf figures/
-cp $DIR/snplot-Middelpos-tf1-E_y.pdf figures/
+cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures
+cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures
+cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures
+cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures
+cp $DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_x.pdf figures
+cp $DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_y.pdf figures
 git add -A figures/
 git commit -a -m "updated figures"
 git push
