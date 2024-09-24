fb89dcfb2ae24f0205bf7f2d1f431c5295fe02fc
Bob Weigel <Robert.S.Weigel@gmail.com>
2024-09-24 15:01:51 -0400

    updated figures

--- cpfigs.sh
+++ cpfigs.sh
@@ -2,10 +2,10 @@ git pull
 DIR=../tfs-20120712-20121107/figures
 cp $DIR/tsplot-original-Middelpos-tf1.pdf figures/
 cp $DIR/dftplot-original-averaged-magnitudes-Middelpos-tf1.pdf figures/
-cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures
-cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures
-cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures
-cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures
+cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures/
+cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures/
+cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures/
+cp $DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf figures/
 cp $DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_x.pdf figures
 cp $DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_y.pdf figures
 git add -A figures/
