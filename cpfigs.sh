c3efc7c701eb89469d9f9c1fa74ddb5576eedf77
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-04-11 09:37:20 -0400

    updated figures

--- cpfigs.sh
+++ cpfigs.sh
@@ -8,6 +8,10 @@ cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_yx.pdf" figures/
 cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_yy.pdf" figures/
 cp "$DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_x.pdf" figures
 cp "$DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_y.pdf" figures
+DIR2=../../KAP03/KAP103/tfs-20031108-20031118/figures
+cp "$DIR2/tsplot-original-KAP103-tf1.pdf" figures/
+cp "$DIR2/dftplot-original-averaged-magnitudes-KAP103-tf1.pdf" figures/
+
 git add -A figures/
 git commit -a -m "updated figures"
 git push
