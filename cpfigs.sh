a55d1fcec890eea8cc9982f895ce71304e7bca7c
Bob Weigel <Robert.S.Weigel@gmail.com>
2025-07-17 17:02:15 -0400

    updated figures

--- cpfigs.sh
+++ cpfigs.sh
@@ -8,7 +8,7 @@ cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_yx.pdf" figures/
 cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_yy.pdf" figures/
 cp "$DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_x.pdf" figures
 cp "$DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_y.pdf" figures
-DIR2=../../KAP03/KAP103/tfs-20031108-20031118/figures
+DIR2=../../KAP03/KAP103/tfs-20031108-20031204/figures
 cp "$DIR2/tsplot-original-KAP103-tf1.pdf" figures/
 cp "$DIR2/dftplot-original-averaged-magnitudes-KAP103-tf1.pdf" figures/
 cp "$DIR2/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_xx.pdf" figures/
