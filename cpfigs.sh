defa7d0357c9f379711f8a86bbf80ef8db3da70c
Bob Weigel <Robert.S.Weigel@gmail.com>
2026-07-27 13:31:16 -0400

    updated figures

--- cpfigs.sh
+++ cpfigs.sh
@@ -5,21 +5,21 @@ cp $RUNDIR/KAP_Middelpos_map/map.pdf figures/
 DIR=$RUNDIR/data/Middelpos/tfs-20120712-20120904/figures
 cp "$DIR/tsplot-original-Middelpos-tf1.pdf" figures/
 cp "$DIR/dftplot-original-averaged-magnitudes-Middelpos-tf1.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1\;Middelpos-tf3-Z_xx.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1\;Middelpos-tf3-Z_xy.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1\;Middelpos-tf3-Z_yx.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1\;Middelpos-tf3-Z_yy.pdf" figures/
-cp "$DIR/snplot-Middelpos-tf1\;Middelpos-tf3-E_x.pdf" figures/
-cp "$DIR/snplot-Middelpos-tf1\;Middelpos-tf3-E_y.pdf" figures/
+cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf" figures/
+cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xy.pdf" figures/
+cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_yx.pdf" figures/
+cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_yy.pdf" figures/
+cp "$DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_x.pdf" figures/
+cp "$DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_y.pdf" figures/
 
 
 cp $RUNDIR/data/Middelpos/tfs-20120712-20120904/Middelpos-tf1-evalfreq_table.tex tables/
 cp $RUNDIR/data/Middelpos/tfs-20120712-20120904/Middelpos-tf3-evalfreq_table.tex tables/
 
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1\;KAP103-tf3\;KAP103-tf1-Z_xx.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1\;KAP103-tf3\;KAP103-tf1-Z_xy.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1\;KAP103-tf3\;KAP103-tf1-Z_yx.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1\;KAP103-tf3\;KAP103-tf1-Z_yy.pdf" figures/
+cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_xx.pdf" figures/
+cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_xy.pdf" figures/
+cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_yx.pdf" figures/
+cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_yy.pdf" figures/
 
 DIR2=$RUNDIR/data/KAP03/KAP103/tfs-20031108-20031204/figures
 cp "$DIR2/tsplot-original-KAP103-tf1.pdf" figures/
