0c569b0a49c5034c5a53d38a251dad97087c76fe
Bob Weigel <Robert.S.Weigel@gmail.com>
2026-07-28 14:49:49 -0400

    updated figures

--- cpfigs.sh
+++ cpfigs.sh
@@ -2,28 +2,30 @@ git pull
 RUNDIR=../tflab/runs
 cp $RUNDIR/KAP_Middelpos_map/map.pdf figures/
 
-DIR=$RUNDIR/data/Middelpos/tfs-20120712-20120904/figures
-cp "$DIR/tsplot-original-Middelpos-tf1.pdf" figures/
-cp "$DIR/dftplot-original-averaged-magnitudes-Middelpos-tf1.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xy.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_yx.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_yy.pdf" figures/
-cp "$DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_x.pdf" figures/
-cp "$DIR/snplot-Middelpos-tf1;Middelpos-tf3-E_y.pdf" figures/
-
-
-cp $RUNDIR/data/Middelpos/tfs-20120712-20120904/Middelpos-tf1-evalfreq_table.tex tables/
-cp $RUNDIR/data/Middelpos/tfs-20120712-20120904/Middelpos-tf3-evalfreq_table.tex tables/
-
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_xx.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_xy.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_yx.pdf" figures/
-cp "$DIR/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_yy.pdf" figures/
-
+DIR0=$RUNDIR/data/Middelpos/tfs-20120712-20120904
+DIR1=$DIR0/figures
 DIR2=$RUNDIR/data/KAP03/KAP103/tfs-20031108-20031204/figures
+
+cp "$DIR1/tsplot-original-Middelpos-tf1.pdf" figures/
+cp "$DIR1/dftplot-original-averaged-magnitudes-Middelpos-tf1.pdf" figures/
+
 cp "$DIR2/tsplot-original-KAP103-tf1.pdf" figures/
 cp "$DIR2/dftplot-original-averaged-magnitudes-KAP103-tf1.pdf" figures/
+
+cp "$DIR1/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xx.pdf" figures/
+cp "$DIR1/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_xy.pdf" figures/
+cp "$DIR1/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_yx.pdf" figures/
+cp "$DIR1/zplot-magnitude_phase-Middelpos-tf1;Middelpos-tf3-Z_yy.pdf" figures/
+cp "$DIR1/snplot-Middelpos-tf1;Middelpos-tf3-E_x.pdf" figures/
+cp "$DIR1/snplot-Middelpos-tf1;Middelpos-tf3-E_y.pdf" figures/
+
+cp "$DIR1/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_xx.pdf" figures/
+cp "$DIR1/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_xy.pdf" figures/
+cp "$DIR1/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_yx.pdf" figures/
+cp "$DIR1/zplot-magnitude_phase-Middelpos-tf1;KAP103-tf3;KAP103-tf1-Z_yy.pdf" figures/
+
+cp $DIR0/Middelpos-tf1-evalfreq_table.tex tables/
+cp $DIR0/Middelpos-tf3-evalfreq_table.tex tables/
 
 git add -A figures/
 git commit -a -m "updated figures"
